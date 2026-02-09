89b9964a672b67d7694192ba715c3e24fbee19e0
cpiaseque <cpiaseque.work@gmail.com>
2026-02-09 13:14:35 +0300

    fix cors

--- configs/config.py
+++ configs/config.py
@@ -20,3 +20,19 @@ def getenv(name: str) -> str:
 RMQ_URL = getenv("RMQ_URL")
 ALERTS_EXCHANGE = getenv("ALERTS_EXCHANGE")
 ALERT_STATUS_QUEUE = getenv("ALERT_STATUS_QUEUE")
+
+
+ALLOWED_HOSTS = [
+    url.strip()
+    for url in os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+    if url.strip()
+]
+
+
+CORS_ALLOWED_ORIGINS = [
+    url.strip()
+    for url in os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+    if url.strip()
+]
+
+
--- main.py
+++ main.py
@@ -17,3 +17,16 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router)
+
+app.add_middleware(
+    TrustedHostMiddleware,
+    allowed_hosts=config.ALLOWED_HOSTS,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=config.CORS_ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
