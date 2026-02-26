bd2938bccdc993a2ceb71a5d5740926e0b25f1e0
Литвинюк Тимофей <108889278+4eburaWka@users.noreply.github.com>
2026-02-26 07:54:09 +0300

    feat: multiple websocket for one author (#22)
    * start rewrite
    
    * feat: multiple websocket for one author

--- alerts/routers.py
+++ alerts/routers.py
@@ -1,3 +1,4 @@
+import asyncio
 import jwt
 
 from fastapi import APIRouter, WebSocket, WebSocketException, status
@@ -16,13 +17,16 @@ async def websocket_alert_endpoint(websocket: WebSocket, widget_token: str):
         widget_token_info = decode_custom_jwt(widget_token)
     except jwt.InvalidTokenError as e:
         raise WebSocketException(status.WS_1008_POLICY_VIOLATION, "invalid widget_token")
-    
+
     if not await check_widget_token(widget_token_info):
         raise WebSocketException(status.WS_1008_POLICY_VIOLATION, "invalid widget_token")
 
     await ws_manager.connect(widget_token_info.author_id, websocket)
     exchange = await rabbitmq_consumer.create_listener(widget_token_info.author_id)
-    await ws_manager.listen(
-        widget_token_info.author_id,
-        get_ws_messages_handler(widget_token_info.author_id, exchange),
+    asyncio.create_task(
+        ws_manager.listen(
+            widget_token_info.author_id,
+            websocket,
+            get_ws_messages_handler(widget_token_info.author_id, exchange),
+        )
     )
--- alerts/websocket.py
+++ alerts/websocket.py
@@ -1,52 +1,88 @@
 import asyncio
-
 import logging
-from typing import Dict
-from fastapi import WebSocket, WebSocketDisconnect
+
+from fastapi import WebSocketException, status
+from fastapi.websockets import WebSocket, WebSocketDisconnect, WebSocketState
 
 
-class WebSocketManager:
+class WSManager:
     def __init__(self):
-        self.connections: Dict[int, WebSocket] = {}
         self.lock = asyncio.Lock()
-
+        self.connections: dict[int, list[WebSocket]] = {}
 
     async def connect(self, author_id: int, websocket: WebSocket):
         await websocket.accept()
+        if websocket.client_state != WebSocketState.CONNECTED:
+            logging.error(f"Websocket accept error: websocket status {websocket.client_state}")
+            raise WebSocketException(
+                status.WS_1006_ABNORMAL_CLOSURE, f"Websocket accept error: websocket status {websocket.client_state}"
+            )
         async with self.lock:
-            self.connections[author_id] = websocket
-    
+            if author_id not in self.connections:
+                self.connections[author_id] = []
+            self.connections[author_id].append(websocket)
+        logging.info(f"User {author_id} connected")
 
-    async def listen(self, author_id: int, on_message: callable = None):
+    async def disconnect(self, author_id: int, websocket: WebSocket):
+        async with self.lock:
+            if author_id in self.connections:
+                try:
+                    self.connections[author_id].remove(websocket)
+                    await websocket.close()
+                except ValueError:
+                    pass
+
+                if not self.connections[author_id]:
+                    del self.connections[author_id]
+                    logging.info(f"Author {author_id} fully disconnected")
+                else:
+                    logging.info(
+                        f"Author {author_id} partially disconnected. Remaining: {len(self.connections[author_id])}"
+                    )
+
+    async def clear_disconnected(self, author_id: int):
         if author_id not in self.connections:
-            logging.warning(f"Ws connection to author {author_id} not exists")
             return
-        
-        websocket = self.connections[author_id]
-        while True:
-            try:
+        async with self.lock:
+            self.connections[author_id] = list(
+                filter(lambda ws: ws.client_state == WebSocketState.CONNECTED, self.connections[author_id])
+            )
+
+    def is_author_connected(self, author_id: int) -> bool:
+        if author_id not in self.connections:
+            return False
+        if not list(filter(lambda ws: ws.client_state == WebSocketState.CONNECTED, self.connections[author_id])):
+            return False
+        return True
+
+    async def broadcast(self, author_id: int, data: dict):
+        if not self.is_author_connected(author_id):
+            logging.error(f"Author not in connections list")
+            return
+        await self.clear_disconnected(author_id)
+
+        async with self.lock:
+            tasks = []
+
+            for ws in self.connections[author_id]:
+                tasks.append(ws.send_json(data))
+
+        await asyncio.gather(*tasks, return_exceptions=False)
+
+    async def listen(self, author_id: int, websocket: WebSocket, on_message: callable = None):
+        try:
+            while True:
                 data = await websocket.receive_json()
                 if on_message:
                     await on_message(data)
-            except (WebSocketDisconnect, RuntimeError) as e:
-                logging.error(f"Error when try to listen websocat {author_id}: {e}")
-                await self.disconnect(author_id)
-
-    async def disconnect(self, author_id: int):
-        await self.connections[author_id].close()
-        async with self.lock:
-            if author_id in self.connections:
-                del self.connections[author_id]
-
-    async def send_to_author(self, author_id: int, data: dict):
-        if author_id not in self.connections:
-            logging.error("Author not in connections")
-            return
-        try:
-            await self.connections[author_id].send_json(data)
         except WebSocketDisconnect:
-            await self.disconnect(author_id)
-            raise WebSocketDisconnect
-        
+            logging.error(f"Websocket disconnected for author {author_id}")
+            await self.clear_disconnected(author_id)
+        except Exception as e:
+            logging.error(f"Unknown exception when received message {e}", exc_info=True)
+        finally:
+            await self.disconnect(author_id, websocket)
+            raise WebSocketException(status.WS_1002_PROTOCOL_ERROR, "incorrect message")
 
-ws_manager = WebSocketManager()
+
+ws_manager = WSManager()
