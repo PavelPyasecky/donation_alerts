3c6b7f74413f07586c8d1efeeb040cff866b4ca6
4eburaWka <prikpasa14@gmail.com>
2026-03-13 15:02:16 +0300

    feat: get alert settings with interval and send to alert widget

--- alerts/models.py
+++ alerts/models.py
@@ -4,7 +4,7 @@ from enum import Enum
 from pydantic import BaseModel, Field
 
 
-class AlertSettings(BaseModel):
+class PageSettings(BaseModel):
     max_message_length: int
     font_size: int
     min_amount: float
@@ -19,7 +19,7 @@ class Alert(BaseModel):
     donor_name: str
     video_url: str
     sound_url: str
-    alert_settings: AlertSettings
+    alert_settings: PageSettings
     donation_id: int | None = Field(None)
     timestamp: datetime.datetime
 
@@ -74,3 +74,70 @@ class Campaign(BaseModel):
     start_at: datetime.datetime
     end_at: datetime.datetime
     is_active_now: bool = Field(False)
+
+
+class RabbitMessageTypes(Enum):
+    event = "event"
+    update = "update"
+
+
+class SkipAlert(BaseModel):
+    author_id: int
+    donation_id: int
+
+
+class RabbitMessage(BaseModel):
+    type_: RabbitMessageTypes = Field(alias="type")
+    action: str
+    data: Alert | Campaign | SkipAlert | list["AlertSetting"]
+
+
+class AlertSetting(BaseModel):
+    id: int
+    is_active: bool = Field(False)
+    type_: str = Field(alias="type")
+
+    activation_type: str
+    activation_amount: str
+    sound_duration: int
+    message_duration: int
+    appear_effect: str
+    disappear_effect: str
+    image_position: str
+
+    header_template: str
+    header_font: str
+    header_main_color: str
+    header_additional_color: str
+    header_font_size: int
+    header_animation_type: str
+    header_animation_objects: str
+    header_animate_all_words: bool = Field(False)
+    header_shadow_size: int
+    header_contour_color: str
+    header_enable_contour: bool = Field(False)
+
+    body_template: str
+    body_font: str
+    body_main_color: str
+    body_font_size: int
+    body_shadow_size: int
+    body_contour_color: str
+    body_enable_contour: bool = Field(False)
+    body_max_message_length: int
+
+    content_type: str
+    content: str | None = Field(None)
+    content_animation_type: str
+
+    music_file: str | None = Field(None)
+    music_volume: int
+
+    speech_synthesis_is_enabled: bool | None = Field(None)
+    speech_synthesis_min_amount: str | None = Field(None)
+    speech_synthesis_volume: int | None = Field(None)
+    speech_synthesis_start_type: str | None = Field(None)
+    speech_synthesis_voice: int | None = Field(None)
+
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
--- alerts/websocket.py
+++ alerts/websocket.py
@@ -4,6 +4,9 @@ import logging
 from fastapi import WebSocketException, status
 from fastapi.websockets import WebSocket, WebSocketDisconnect, WebSocketState
 
+from alerts.grpc import alert_settings_grpc_client
+from alerts.models import RabbitMessage, RabbitMessageTypes
+
 
 class WSManager:
     def __init__(self):
@@ -82,6 +85,22 @@ class WSManager:
             logging.error(f"Unknown exception when received message {e}", exc_info=True)
         finally:
             await self.disconnect(author_id, websocket)
+    
+    async def start_schedule_task(self, interval_seconds: int, action: callable, **kwargs):
+        return asyncio.create_task(action(interval_seconds=interval_seconds, **kwargs))
+    
+    async def _get_and_send_alert_settings(self, interval_seconds: int, **kwargs):
+        author_id = kwargs.get("author_id")
+        group_id = kwargs.get("group_id")
+        while True:
+            if not self.is_author_connected(author_id):
+                break
+
+            alerts_settings = await alert_settings_grpc_client.get_alert_settings_list_by_author_id(author_id, group_id)
+            message = RabbitMessage(type=RabbitMessageTypes.update, action="alert_settings", data=alerts_settings)
+
+            await self.broadcast(author_id, message.model_dump(mode="json", by_alias=True))
+            await asyncio.sleep(interval_seconds)
 
 
 ws_alerts_manager = WSManager()
