80576b16b4ba78a270a8e0bee89696e2fb2c5920
4eburaWka <prikpasa14@gmail.com>
2026-04-01 22:39:56 +0300

    remove comments

--- alerts/poll_state.py
+++ alerts/poll_state.py
@@ -4,15 +4,11 @@ from dataclasses import dataclass, field
 
 @dataclass
 class TimestampPollState:
-    """Мутабельный курсор updated_at для периодических gRPC-опросов."""
-
     updated_at: datetime.datetime
 
 
 @dataclass
 class ConnectedGroupsPollState:
-    """Состояние опроса connected groups + курсор для get_alert_settings_groups."""
-
     connected_group_ids: list[int] = field(default_factory=list)
     updated_at: datetime.datetime = field(
         default_factory=lambda: datetime.datetime.fromtimestamp(0, datetime.timezone.utc)
